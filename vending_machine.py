539bb4d40242cc1fdf0e4e7e1f183f4d34eb145f
kittymcdonagh <kittymcdonagh@gmail.com>
2019-07-02 07:45:13 +0000

    9 tested in vending_machine

--- vending_machine.py
+++ vending_machine.py
@@ -15,7 +15,14 @@ def get_change(amount):
        
     if amount in [100, 50, 20, 10, 5, 2, 1]:
         return [amount]
-    
+
+# Note: The program will go through the coin loop one after the other. When the
+# amount is changed, the next coin it picks from the list, is the one after the
+# last one it checked. So if we use 9 as the amount, the first coin to be 
+# appended will be the 5, amount will be reduced to 4; next coin picked up will
+# be 2, amount will be reduced to 2; next coin to be picked up will be 1. So it
+# will return 5, 2, 1 for 9, instead of 5, 2, 2.
+
     change = []
     for coin in [100, 50, 20, 10, 5, 2, 1]:
         if coin <= amount:
@@ -32,9 +39,8 @@ test_are_equal(get_change(20), [20])
 test_are_equal(get_change(50), [50])
 test_are_equal(get_change(100), [100])
 test_are_equal(get_change(7), [5, 2])
-test_are_equal(get_change(3), [2, 1])
-test_are_equal(get_change(15), [10, 5])
-test_are_equal(get_change(25), [20, 5])
+test_are_equal(get_change(9), [5, 2, 2])
+
 
 
 
