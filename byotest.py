df96da2193e549dc53c4e8229952eae03ab47e63
kittymcdonagh <kittymcdonagh@gmail.com>
2019-07-02 06:36:27 +0000

    byotest updated from ci solution code

--- byotest.py
+++ byotest.py
@@ -1,34 +1,76 @@
+"""
+Copied this from CI Solution Code before starting mini project to ensure I started with no bugs.
 
-# Testing Functions
+Solution to the third challenge in the `Build Your Own Test Framework` unit 
+found in the `Test Driven Development with Python lesson`
+NOTE: The solution found in this file is one of the many potential solutions
+that can be used to achieve the end result expected by the challenge in the
+lesson.
+"""
+
+
+def test_are_equal(actual, expected):
+    """
+    Test that two values are equal. Raises AssertionError if both values are
+    not equal.
+    `actual` is the actual value produced
+    `expected` is the value that was supposed to be produced
+    """
+    assert expected == actual, "Expected {0}, got {1}".format(
+        expected, actual)
+
 
 def test_not_equal(a, b):
-    assert a != b, "Did not expect {0}, but got {1}".format(a, b)
-    
-def test_between(actual, lower_limit, upper_limit):
-    assert lower_limit <= actual <= upper_limit, "{0} is less than or equal to {1} or equal to or greater than {2}".format(actual, lower_limit, upper_limit)
-    
-def test_is_in(item, collection):
-    assert item in collection, "{0} does not contain {1}".format(collection, item)
-    
-def test_not_in(item, collection):
-    assert item not in collection, "{0} contains {1}".format(collection, item)
-    
-
-# Run test on test functions
-
-test_not_equal(2, 3)
-
-test_is_in(5, [1, 2, 3, 4, 5])
-
-test_not_in(6, [1, 2, 3, 4, 5])
-
-test_between(6, 4, 9)
-
-print("All tests have passed!")
+    """
+    Test that two values are not equal. Raises AssertionError if both values
+    are not equal.
+    `a` is the actual value produced
+    `b` is the value that was supposed to be produced
+    """
+    assert a != b, "Did not expect {0} but got {1}".format(a, b)
 
 
+def test_is_in(collection, item):
+    """
+    Check to ensure that a given collection contains a given value. Raises
+    AssertionError if `item` is not in `collection`
+    `collection` is the collection to be tested
+    `item` is the item that is being searched for
+    """
+    assert item in collection, "{0} does not contain {1}".format(
+        collection, item)
 
 
+def test_not_in(collection, item):
+    """
+    Check to ensure that a given collection does not contain a given value.
+    Raises AssertionError if the `item` is found in `collection`
+    `collection` is the collection in question
+    `item` is the thing that we want to check for
+    """
+    assert item not in collection, "{0} is not in {1}".format(
+        item, collection)
 
 
+def test_between(upper_limit, lower_limit, actual):
+    """
+    Check to ensure that a number is between two other numbers. Raises
+    AssertionError if the number is not between the other two numbers
+    """
+    assert lower_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
 
+
+# Test to fail the `test_are_equal` function
+# test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+
+# Test to fail the `test_not_equal` function
+# test_not_equal(0, 0)
+
+# Test to fail the `test_is_in` function
+# test_is_in([1], 2)
+
+# Test to fail the `test_not_in` function
+# test_not_in([1], 1)
+
+# Test to fail the `test_between` function
+test_between(10, 1, 200)
